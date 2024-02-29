e843c4faac9fad36f9a3ccc3831a65f909fc3eaf
Pratiksha Darekar <113767661+PratikshaD45@users.noreply.github.com>
2024-02-29 22:25:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of bit differences/sum-of-bit-differences.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            System.out.println(new Solution().sumBitDifferences(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+//User function template for C++
+ class Solution {
+    long sumBitDifferences(int[] arr, int n) {
+        long ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if ((arr[j] & (1 << i)) != 0) {
+                    count++;
+                }
+            }
+            ans += 2L * count * (n - count);
+        }
+        return ans;
+    }
+}
+
+
+    
